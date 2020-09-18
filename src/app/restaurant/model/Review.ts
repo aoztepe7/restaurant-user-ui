0d96650e14fd7ea43c865fa18addefa12e933e28
Ali Oztepe <aliztp7@gmail.com>
2020-09-18 23:03:24 +0300

    user ui all features is done

---
+++ src/app/restaurant/model/Review.ts
@@ -0,0 +1,28 @@
+import {Restaurant} from '../../restaurant/model/Restaurant';
+
+export class Review {
+  constructor(public uuid: string,
+              public restaurant: Restaurant,
+              public visitDate: string,
+              public answerRequired: boolean,
+              public comment: string) {
+  }
+}
+
+export class ReviewList {
+  constructor(public code: number,
+              public message: string,
+              public reviews: Pagination) {
+  }
+}
+
+export class Pagination {
+  constructor(public content: Array<Review>,
+              public first: boolean,
+              public last: boolean,
+              public number: number,
+              public numberOfElements: number,
+              public totalElements: number,
+              public totalPages: number) {
+  }
+}
